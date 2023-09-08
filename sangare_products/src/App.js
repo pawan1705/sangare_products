1b4d37eefe01f213cfd0a199d1a28ef1d9a8d798
pawan1705 <pawan.sangare05@gmail.com>
2023-09-08 15:16:01 +0530

    08sep

---
+++ sangare_products/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+// import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ sangare_products/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../features/navbar/navbar'
+import ProductList from '../features/product-list/ProductList'
+function Home() {
+  return (
+    <div>
+        <Navbar>
+            <ProductList></ProductList>
+        </Navbar>
+    </div>
+  )
+}
+
+export default Home;
